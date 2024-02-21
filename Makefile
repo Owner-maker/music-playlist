5812e77ba8edc327e8ae3bea18efc5399c8115dd
Artem Lisitsyn <lisitsyn.artem.d@gmail.com>
2024-02-21 14:15:11 +0400

    added config and logger

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	go run ./cmd --config="./config/local.yaml"
---
+++ cmd/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log/slog"
+	"music-playlist/internal/config"
+	"music-playlist/pkg/logger/handlers/slogpretty"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+	log.Info("starting application", slog.Any("env", &cfg))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{SlogOpts: &slog.HandlerOptions{Level: slog.LevelDebug}}
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
